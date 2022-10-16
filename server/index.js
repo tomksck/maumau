297254404a1bf2b02cc48b6bf9261639583d0f0f
Tom Kosacki <tom.kosacki@gmail.com>
2022-10-16 18:03:12 +0200

    added nunjucks + routing

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import nunjucks from 'nunjucks';
 import bodyParser from 'body-parser';
 import GameController from './src/controllers/game_controller.js';
 
@@ -8,70 +9,114 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+nunjucks.configure('server/views', {
+  autoescape: true,
+  express: app
+});
+
 const game = new GameController();
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
+});
 
-  app.get('/', (req, res) => {
-    res.send('Welcome to MauMau!');
-  });
+app.get('/', async (req, res) => {
+  game.removeAllListeners();
+  const data = { title: 'MauMau', message: 'Welcome to MauMau!', layout: 'layout.njk', url: '/game' };
+  res.render('index.njk', data);
+});
 
-  app.get('/game', (req, res) => {
-    game.on('newGame', () => {
-      res.send('New game started!Players:' + game.getPlayerNames().join(', ') + '<br>Press enter when ready...' + '<br><a href="/game/turn">Next Turn</a>');
-    });
-    game.newGame(2);
+app.get('/game', async (req, res) => {
+  game.removeAllListeners();
+  game.once('newGame', () => {
+    const data = {
+      title: 'MauMau',
+      message: 'New game started!',
+      players: game.getPlayerNames(),
+      layout: 'layout.njk',
+      url: '/game/turn'
+    };
+    res.render('game.njk', data);
   });
+  game.newGame(2);
+});
 
-  app.get('/game/turn', (req, res) => {
-    game.on('newTurn', () => {
-      res.send("It's " + game.getPlayerName() + "'s turn!" + '<br>Press enter when ready...<br><a href="/game/turn/throw">Ready</a>');
-    });
-    game.nextTurn();
+app.get('/game/turn', async (req, res) => {
+  game.removeAllListeners();
+  game.once('newTurn', () => {
+    const data = {
+      layout: 'layout.njk',
+      message: 'New turn started!',
+      player: game.getPlayerName(),
+      url: '/game/turn/throw',
+      showCards: false,
+      text: 'Show Cards'
+    };
+    res.render('turn.njk', data);
   });
+  game.nextTurn();
+});
 
-  app.get('/game/turn/throw', (req, res) => {
-    res.send(
-      'Table Card:' +
-        game.getTableCard().toString() +
-        '<br>Your Cards:' +
-        game
-          .getPlayerCards()
-          .map((card, index) => {
-            return '<br><a href=/game/turn/throw/' + index + '>' + card.toString() + '</a>';
-          })
-          .join('') +
-        '<br><a href="/game/turn/take">Take</a>' +
-        '<br>Please select the card to throw or take a card.'
-    );
-  });
+app.get('/game/turn/throw', async (req, res) => {
+  game.removeAllListeners();
+  const data = {
+    player: game.getPlayerName(),
+    message: 'Please throw a card or Take one',
+    layout: 'layout.njk',
+    showCards: true,
+    tableCard: game.getTableCard().toString(),
+    cardUrl: '/game/turn/throw/',
+    cards: game.getPlayerCards().map((card, index) => {
+      return card.toString();
+    }),
+    url: '/game/turn/take'
+  };
+  res.render('turn.njk', data);
+});
 
-  app.get('/game/turn/throw/:card', (req, res) => {
-    game.on('threwCard', (player, card) => {
-      res.send(player.getName() + ' threw ' + card.toString() + '<br><a href="/game/turn">Next Turn</a>');
-    });
-    game.on('cardNotThrown', () => {
-      res.send('Card not thrown! Please try again. <br><a href="/game/turn/throw">Back</a>');
-    });
-    game.on('playerWon', (player) => {
-      res.send(player.getName() + ' won! <br><a href="/game">New Game</a>');
-    });
-    game.throwCard(parseInt(req.params.card));
+app.get('/game/turn/throw/:card', async (req, res) => {
+  game.removeAllListeners();
+  game.once('threwCard', (player, card) => {
+    const data = {
+      player: game.getPlayerName(),
+      layout: 'layout.njk',
+      showCards: false,
+      message: `${player.getName()} threw ${card.toString()}`,
+      url: '/game/turn',
+      text: 'Next Turn'
+    };
+    res.render('turn.njk', data);
   });
+  game.once('cardNotThrown', () => {
+    const data = {
+      player: game.getPlayerName(),
+      layout: 'layout.njk',
+      showCards: false,
+      message: 'Card not thrown!',
+      url: '/game/turn/throw',
+      text: 'Retry'
+    };
+    res.render('turn.njk', data);
+  });
+  game.once('playerWon', (player) => {
+    res.send(player.getName() + ' won! <br><a href="/game">New Game</a>');
+  });
+  game.throwCard(parseInt(req.params.card));
+});
 
-  app.get('/game/turn/take', (req, res) => {
-    game.on('tookCard', (player, card) => {
-      res.send(player.getName() + ' took ' + card.toString() + '<br><a href="/game/turn">Next Turn</a>');
-    });
-    game.takeCard();
+app.get('/game/turn/take', async (req, res) => {
+  game.removeAllListeners();
+  game.once('tookCard', (player, card) => {
+    res.send(player.getName() + ' took ' + card.toString() + '<br><a href="/game/turn">Next Turn</a>');
   });
+  game.takeCard();
+});
 
-  app.get('/game/:playerCount', (req, res) => {
-    const game = new GameController();
-    game.on('newGame', () => {
-      res.send('New game started!');
-    });
-    game.newGame(parseInt(req.params.playerCount));
+app.get('/game/:playerCount', async (req, res) => {
+  game.removeAllListeners();
+  const game = new GameController();
+  game.once('newGame', () => {
+    res.send('New game started!');
   });
+  game.newGame(parseInt(req.params.playerCount));
 });
